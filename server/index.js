190f68ad055054b252ecb0f38ec1284fc7c856b8
Juan Cetina <juan.cetina@correo.uis.edu.co>
2021-10-17 16:50:46 -0500

    commit configuracion error en bd

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3977;
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
 
-mongoose.set("useFindeAndModify", false); //Evita errores cuando se hace una peticion a la base de datos
+// mongoose.set("useFindAndModify", false); //Evita errores cuando se hace una peticion a la base de datos
 // mongoose.connect(`mongodb://localhost:27017/baseDatos`);
 mongoose.connect( //Conectar con nuestra base datos
     `mongodb://${IP_SERVER}:${PORT_DB}/baseDatos`,
